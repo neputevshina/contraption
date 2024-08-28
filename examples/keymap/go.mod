b600a9c39f7071c0f20d1d15a4f4b28c30e41999
neputevshina <51914743+neputevshina@users.noreply.github.com>
2024-08-28 11:57:16 +0400

    Binsearch for capsize, fix noaligner size, some examples

---
+++ examples/keymap/go.mod
@@ -0,0 +1,29 @@
+module github.com/neputevshina/contraption/examples/keymap
+
+go 1.23.0
+
+replace github.com/neputevshina/contraption => ../../
+
+require (
+	github.com/go-gl/gl v0.0.0-20231021071112-07e5d0ea2e71
+	github.com/go-gl/glfw/v3.3/glfw v0.0.0-20240506104042-037f3cc74f2a
+	github.com/golang/freetype v0.0.0-20170609003504-e2365dfdc4a0
+	github.com/h2non/filetype v1.1.3
+	github.com/neputevshina/contraption v0.0.0-20240826020825-d198aacddaff
+	github.com/neputevshina/geom v0.0.0-20231211132238-0cb016e95618
+	github.com/neputevshina/nanovgo v0.0.0-20240706040057-6232d7963035
+	github.com/shibukawa/nanovgo v0.0.0-20160822101109-9141d09b3652
+	golang.design/x/clipboard v0.7.0
+	golang.org/x/exp v0.0.0-20240823005443-9b4947da3948
+	golang.org/x/exp/shiny v0.0.0-20240823005443-9b4947da3948
+	golang.org/x/image v0.19.0
+)
+
+require (
+	github.com/gopherjs/gopherjs v1.17.2 // indirect
+	github.com/goxjs/gl v0.0.0-20230705020350-37525f4d9d35 // indirect
+	golang.org/x/mobile v0.0.0-20231127183840-76ac6878050a // indirect
+	golang.org/x/sys v0.19.0 // indirect
+	golang.org/x/text v0.17.0 // indirect
+	honnef.co/go/js/console v0.0.0-20150119023344-105276c43558 // indirect
+)
--- oscilloscope.go
+++ oscilloscope.go
@@ -13,16 +13,22 @@ type Oscilloscope struct {
 func (wo *World) displayOscilloscope() Sorm {
 	if wo.Oscilloscope.on {
 		return wo.Compound(
-			wo.Rectangle(wo.Wwin+1, wo.Hwin+1).Fill(hexpaint(`#00000060`)),
-			wo.Canvas(-1, -1, func(vgo *nanovgo.Context, rect geom.Rectangle) {
-				vgo.SetFillColor(hex(`#ffffff`))
-				vgo.SetFontSize(12)
-				vgo.SetTextAlign(nanovgo.AlignLeft)
-				y := float32(0.0)
-				for _, s := range collect(wo.Events.Trace, func(e EventPoint) string { return sprint(e.String()) }) {
-					vgo.Text(12, 14+y, s)
-					y += 14
-				}
+			// wo.Limit(wo.Wwin+1, wo.Hwin+1),
+			wo.Compound(
+				wo.Rectangle(wo.Wwin+1, wo.Hwin+1).Fill(hexpaint(`#00000060`)), // FIXME Must be -1, -1, is not working even with limit.
+				wo.Canvas(-1, -1, func(vgo *nanovgo.Context, rect geom.Rectangle) {
+					vgo.SetFillColor(hex(`#ffffff`))
+					vgo.SetFontSize(12)
+					vgo.SetTextAlign(nanovgo.AlignLeft)
+					y := float32(0.0)
+					for _, s := range collect(wo.Events.Trace, func(e EventPoint) string { return sprint(e.String()) }) {
+						vgo.Text(12, 14+y, s)
+						y += 14
+					}
+				})).Cond(func(m Matcher) {
+				println(m.Rect())
+				// FIXME {0y0, 0y0}
+				m.Match(`.*`)
 			}))
 	}
 	return wo.Void(0, 0)
